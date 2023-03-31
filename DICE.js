d0d476cac29579033ea56cac69c688c5eb295f23
madmeche <madeleine.meche@gmail.com>
2023-03-31 16:20:51 -0500

    updated dice, rolling, and display

---
+++ DICE.js
@@ -0,0 +1,16 @@
+var dice = {
+    sideSix: 6,
+    sideTen: 10,
+    roll: function() {
+    //   var randomNumber = Math.floor(Math.random() * this.sideSix); //error thrown! must add '1' to not get a zero.
+      var randomNumber6 = Math.floor(Math.random() * this.sideSix) + 1;
+      var randomNumber10 = Math.floor(Math.random() * this.sideTen) + 1; //error thrown! rolled same number over again when in line with sideSix
+      return randomNumber6;
+      return randomNumber10; //error thrown! rolled same number over again when in line with sideSix
+    },
+  }
+  
+button.onclick = function() {
+  var result= dice.roll()
+  console.log(result)
+}
