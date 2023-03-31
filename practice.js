338b4744a794749fd8de1d840247dc41543d8f20
madmeche <madeleine.meche@gmail.com>
2023-03-31 12:17:58 -0500

    created branches

---
+++ practice.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Blueberry"];
+console.log(fruits)
+delete fruits [1]
+console.log(fruits)
+fruits[1] = "Kiwi";
+delete fruits [4]
+fruits[4] = "Strawberry"
+
+console.log(fruits)
